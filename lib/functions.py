55651a7c9ebcb3d8663a621685b6c0ba3986328c
LAETITIA-hub <laetitia.kamangu@student.moringaschool.com>
2025-05-07 13:02:12 +0300

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print ("Hello, programmer!")
+    
     pass
 
 def greet(name):
+    print ("Hello, Guido!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+    
     pass
 
 def add(num1, num2):
+    return num1 + num2
+    print(add(45, 55))
+
+   
     pass
 
 def halve(number):
+    return number/2
+    print(divide(100,2))
+    
     pass
